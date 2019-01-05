1c5cd889fe582d09c9b4ea5cf83953420f08ee5d
zie87 <zie.develop@gmail.com>
2019-01-05 19:43:32 +0100

    build fix

--- core/palette.cpp
+++ core/palette.cpp
@@ -4,83 +4,63 @@
 
 #include <SDL.h>
 
-#include <core/sdl_utils.h>
-#include <core/sdl_error.h>
 #include <core/color.h>
 #include <core/modulo.h>
+#include <core/sdl_error.h>
+#include <core/sdl_utils.h>
 
-namespace core
-{
-    Palette::Palette(Palette const&) = default;
-    Palette& Palette::operator=(Palette const&) = default;
-    Palette::Palette(Palette&&) = default;
-    Palette& Palette::operator=(Palette&&) = default;
-    Palette::~Palette() = default;
+namespace core {
+Palette::Palette(Palette const &) = default;
+Palette &Palette::operator=(Palette const &) = default;
+Palette::Palette(Palette &&) = default;
+Palette &Palette::operator=(Palette &&) = default;
+Palette::~Palette() = default;
 
-    Palette::Palette()
-        : mStorage(nullptr)
-    {}
-    
-    constexpr auto paletteDeleter = [](SDL_Palette *palette) {
-        SDL_FreePalette(palette);
-    };
+Palette::Palette() : mStorage(nullptr) {}
 
-    Palette::Palette(size_t colors)
-    {
-        PalettePtr tmp(SDL_AllocPalette(colors));
-        if(!tmp) {
-            throw sdl_error();
-        }
-        mStorage = std::move(tmp);
-    }
-    
-    Palette::value_type const& Palette::operator[](size_type index) const
-    {
-        return mStorage->colors[index];
-    }
+const auto paletteDeleter = [](SDL_Palette *palette) {
+  SDL_FreePalette(palette);
+};
 
-    Palette::value_type& Palette::operator[](Palette::size_type index)
-    {
-        return mStorage->colors[index];
-    }
-    
-    Palette::size_type Palette::Size() const
-    {
-        return (Null() ? 0 : mStorage->ncolors);
-    }
-    
-    Palette::const_iterator Palette::begin() const
-    {
-        return (Null() ? nullptr : mStorage->colors);
-    }
-    
-    Palette::const_iterator Palette::end() const
-    {
-        return (Null() ? nullptr : (mStorage->colors + Size()));
-    }
-    
-    Palette::iterator Palette::begin()
-    {
-        return (Null() ? nullptr : (mStorage->colors));
-    }
-    
-    Palette::iterator Palette::end()
-    {
-        return (Null() ? nullptr : (mStorage->colors + Size()));
-    }
-
-    SDL_Palette& Palette::GetSDLPalette()
-    {
-        return *mStorage;
-    }
-    
-    bool Palette::Null() const
-    {
-        return !static_cast<bool>(mStorage);
-    }
-    
-    bool Palette::operator!() const
-    {
-        return !Null();
-    }
+Palette::Palette(size_t colors) {
+  PalettePtr tmp(SDL_AllocPalette(colors));
+  if (!tmp) {
+    throw sdl_error();
+  }
+  mStorage = std::move(tmp);
 }
+
+Palette::value_type const &Palette::operator[](size_type index) const {
+  return mStorage->colors[index];
+}
+
+Palette::value_type &Palette::operator[](Palette::size_type index) {
+  return mStorage->colors[index];
+}
+
+Palette::size_type Palette::Size() const {
+  return (Null() ? 0 : mStorage->ncolors);
+}
+
+Palette::const_iterator Palette::begin() const {
+  return (Null() ? nullptr : mStorage->colors);
+}
+
+Palette::const_iterator Palette::end() const {
+  return (Null() ? nullptr : (mStorage->colors + Size()));
+}
+
+Palette::iterator Palette::begin() {
+  return (Null() ? nullptr : (mStorage->colors));
+}
+
+Palette::iterator Palette::end() {
+  return (Null() ? nullptr : (mStorage->colors + Size()));
+}
+
+SDL_Palette &Palette::GetSDLPalette() { return *mStorage; }
+
+bool Palette::Null() const { return !static_cast<bool>(mStorage); }
+
+bool Palette::operator!() const { return !Null(); }
+} // namespace core
--- core/point.cpp
+++ core/point.cpp
@@ -5,101 +5,83 @@
 
 #include <core/rect.h>
 
-namespace
-{
-    void test()
-    {
-        constexpr core::Rect r(100, 100, 200, 200);
+namespace {
+void test() {
+  constexpr core::Rect r(100, 100, 200, 200);
 
-        constexpr core::Point a(0, 0);
-        constexpr core::Point b(100, 100);
-        constexpr core::Point c(99, 150);
-        constexpr core::Point d(150, 99);
-        
-        static_assert(!core::PointInRect(r, a), "A in R");
-        static_assert(core::PointInRect(r, b), "B not in R");
-        static_assert(!core::PointInRect(r, c), "C in R");
-        static_assert(!core::PointInRect(r, d), "D in R");
-    }
-    
-    template<class T>
-    constexpr T Sqr(T x)
-    {
-        return x * x;
-    }
+  constexpr core::Point a(0, 0);
+  constexpr core::Point b(100, 100);
+  constexpr core::Point c(99, 150);
+  constexpr core::Point d(150, 99);
+
+  static_assert(!core::PointInRect(r, a), "A in R");
+  static_assert(core::PointInRect(r, b), "B not in R");
+  static_assert(!core::PointInRect(r, c), "C in R");
+  static_assert(!core::PointInRect(r, d), "D in R");
 }
 
-namespace core
-{
-    Point& Point::operator+=(const Point &that)
-    {
-        mX += that.mX;
-        mY += that.mY;
-        return *this;
-    }
+template <class T> constexpr T Sqr(T x) { return x * x; }
+} // namespace
 
-    Point& Point::operator-=(const Point &that)
-    {
-        mX -= that.mX;
-        mY -= that.mY;
-        return *this;
-    }
-
-    Point& Point::operator*=(const Point &that)
-    {
-        mX *= that.mX;
-        mY *= that.mY;
-        return *this;
-    }
-
-    Point& Point::operator/=(const Point &that)
-    {
-        mX /= that.mX;
-        mY /= that.mY;
-        return *this;
-    }
-
-    Point& Point::operator+=(Point::coord_type v)
-    {
-        mX += v;
-        mY += v;
-        return *this;
-    }
-
-    Point& Point::operator-=(Point::coord_type v)
-    {
-        mX -= v;
-        mY -= v;
-        return *this;
-    }
-
-    Point& Point::operator*=(Point::coord_type v)
-    {
-        mX *= v;
-        mY *= v;
-        return *this;
-    }
-
-    Point& Point::operator/=(Point::coord_type v)
-    {
-        mX /= v;
-        mY /= v;
-        return *this;
-    }
-
-    Point::coord_type Manhattan(const Point &lhs, const Point &rhs)
-    {
-        return abs(lhs.X() - rhs.X()) + abs(rhs.Y() - lhs.Y());
-    }
-
-    float Hypot(const Point &lhs, const Point &rhs)
-    {
-        return hypot(lhs.X() - rhs.X(), lhs.Y() - rhs.Y());
-    }
-
-    std::ostream& operator<<(std::ostream &out, const Point &pt)
-    {
-        out << '(' << pt.X() << ',' << pt.Y() << ')';
-        return out;
-    }
+namespace core {
+Point &Point::operator+=(const Point &that) {
+  mX += that.mX;
+  mY += that.mY;
+  return *this;
 }
+
+Point &Point::operator-=(const Point &that) {
+  mX -= that.mX;
+  mY -= that.mY;
+  return *this;
+}
+
+Point &Point::operator*=(const Point &that) {
+  mX *= that.mX;
+  mY *= that.mY;
+  return *this;
+}
+
+Point &Point::operator/=(const Point &that) {
+  mX /= that.mX;
+  mY /= that.mY;
+  return *this;
+}
+
+Point &Point::operator+=(Point::coord_type v) {
+  mX += v;
+  mY += v;
+  return *this;
+}
+
+Point &Point::operator-=(Point::coord_type v) {
+  mX -= v;
+  mY -= v;
+  return *this;
+}
+
+Point &Point::operator*=(Point::coord_type v) {
+  mX *= v;
+  mY *= v;
+  return *this;
+}
+
+Point &Point::operator/=(Point::coord_type v) {
+  mX /= v;
+  mY /= v;
+  return *this;
+}
+
+Point::coord_type Manhattan(const Point &lhs, const Point &rhs) {
+  return abs(lhs.X() - rhs.X()) + abs(rhs.Y() - lhs.Y());
+}
+
+float Hypot(const Point &lhs, const Point &rhs) {
+  return hypot(lhs.X() - rhs.X(), lhs.Y() - rhs.Y());
+}
+
+std::ostream &operator<<(std::ostream &out, const Point &pt) {
+  out << '(' << pt.X() << ',' << pt.Y() << ')';
+  return out;
+}
+} // namespace core
